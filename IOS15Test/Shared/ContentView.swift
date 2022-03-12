9e17199fc23bae2fc6c29f01da88f0e48675934c
Mike716493231 <zth19950326@yahoo.com>
2022-03-12 13:40:16 +0800

    T

---
+++ IOS15Test/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 蜡笔小新的动感超人Macbook Pro Max on 2022/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS15Test/Shared/IOS15TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS15TestApp.swift
+//  Shared
+//
+//  Created by 蜡笔小新的动感超人Macbook Pro Max on 2022/3/12.
+//
+
+import SwiftUI
+
+@main
+struct IOS15TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
