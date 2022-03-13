04ba8f13e6688ac459ac7c6e443fc0568f473837
Mike716493231 <zth19950326@yahoo.com>
2022-03-13 10:25:43 +0800

    Course 2

--- IOS15Test/Shared/ContentView.swift
+++ IOS15Test/Shared/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment:.leading, spacing : 8) {
+            Spacer()
+            
+            Image("Logo 2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            
+            Text("SwiftUI For IOS15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                
+            Text("20sections - 2 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all, 20.0)
+        .frame(height:350)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(radius: 20)
     }
 }
 
